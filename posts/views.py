492498595adf7ed10e065577f74c9a40b17f920a
Megan Paquin <106716810+meganpaquin@users.noreply.github.com>
2022-09-21 23:06:41 -0400

    add test cases, add POST form, GET SQL data

--- posts/views.py
+++ posts/views.py
@@ -3,10 +3,12 @@ from django.views.generic.edit import CreateView
 from .models import Post
 
 class PostListView(ListView):
-    template_name = "posts/lists.html"
+    template_name = "posts/list.html"
+    model = Post
 
 class PostDetailView(DetailView):
     template_name = "posts/detail.html"
+    model = Post
 
 class PostCreateView(CreateView):
     template_name = "posts/new.html"
